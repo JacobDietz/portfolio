b9abfa0c68ee8d44dafa05ba3f2ce7a8a605eabd
Jacob Dietz <jacobdietz2383@gmail.com>
2025-04-01 15:34:48 -0600

    Saving progress, working on navigation implementation

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,44 +1,63 @@
-// import React from "react";
 import contact from "./contact.js"
+import { NavLink } from "react-router"
+import * as motion from "motion/react-client"
 
 
-// function Home() {
-//     console.log("sfds");
-//     return (
-//         <h1 className="bg-red-500">HOME PAGE</h1>
-//     );
-// }
+
+
+
 
 function Home() {
     return (
 
-        <NavBar>
+        <div className="flex-row">
+            <NavBar>
 
-        </NavBar>
+            </NavBar>
+            <section className="bg-blue-200 w-500 h-500">
+                <div id="profile">profile</div>
+
+            </section>
+        </div>
+
     )
 }
 
+export function NavBar() {
 
-function NavBar() {
     return (
-        <nav className="">
+        <div className="h-100">
+            <nav className="fixed">
+                <motion.h1
+                    initial={{ scale: 0 }} animate={{ scale: 1 }}
+                    className="text-9xl font-sans font-medium ml-10 mt-5 tracking-tighter text-white ">Jacob</motion.h1>
 
-            <h1 className="text-9xl font-sans font-medium ml-10 mt-5 tracking-tighter text-white ">Jacob</h1>
-            <div className="flex flex-row">
-                <h2 className="text-5xl font-sans font-medium ml-20 tracking-tighter text-white ">Dietz</h2>
-                <button onClick={contact} className="hover:bg-blue-100 hover:border-red-300 transition-all duration-300 
-                ease-in rounded-xl border-3 font-medium ml-100 p-2 text-red-300-">Contact</button>
-            </div>
+                <motion.h2
+                    initial={{ scale: 0 }} animate={{ scale: 1 }}
+                    className="text-5xl font-sans font-medium ml-20 tracking-tighter text-white ">Dietz</motion.h2>
 
-            <ul className="flex row ">
-                <li className="pl-10 pr-10 hover:bg-red-100">Projects</li>
-            </ul>
-
-        </nav>
+                <ul className="flex row ">
+                    <MainNavLink text={"About Me"}></MainNavLink>
+                    <MainNavLink text={"Projects"}></MainNavLink>
+                    <MainNavLink text={"Resume"}></MainNavLink>
+                    <MainNavLink text={"Contact"}></MainNavLink>
+                </ul>
+            </nav>
+        </div>
     )
-
 }
 
+export function MainNavLink({ text }) {
+    return (
+        <motion.li
+            whileHover={{ scale: 1.1 }}
+            initial={{ scale: 0 }} animate={{ scale: 1 }}
+            whileInView={{ opacity: 1 }}
 
+            className=" text-white font-sans text-3xl font-bold pl-5 pr-5 
+            underline decoration-5 rounded-xl underline-offset-8"><a href="#profile">{text}</a>
+        </motion.li>
+    )
+}
 
 export default Home;
