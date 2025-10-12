8419a575176ba6b88bb99dedcfb12cb8a687affd
Jacob Dietz <jacobdietz2383@gmail.com>
2025-10-11 22:46:35 -0600

    Changed router to new dom router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./Home.jsx";
 import DynamicBckg from "./background.jsx"; 
 import Profile from "./profile.jsx";
