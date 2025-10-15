1adba5ad3c764157c4b8abd2d9f89769bd3ac6f2
Jacob Dietz <jacobdietz2383@gmail.com>
2025-10-15 14:18:33 -0600

    Added github and resume links

--- frontend/src/HomeRough.jsx
+++ frontend/src/HomeRough.jsx
@@ -6,49 +6,57 @@ import DynamicBckg from "./background"
 import { Link } from "react-router"
 import React from "react";
 
-
 function HomeRough() {
     return (
         <div className="">
-          <NavBar />
-          </div>
+            <NavBar />
+        </div>
     );
-  }
-  
+}
+
 
 export function NavBar() {
     return (
-            <nav className="">
-                <motion.h1
-                    initial={{ scale: 0 }} animate={{ scale: 1 }}
-                    className="text-6xl sm:text-8xl md:text-9xl lg:text-9xl font-sans font-medium ml-10 
+        <nav className="">
+            <motion.h1
+                initial={{ scale: 0 }} animate={{ scale: 1 }}
+                className="text-6xl sm:text-6xl md:text-9xl lg:text-9xl font-roboto tracking-tighter font-medium ml-10 
                     mt-5 tracking-tighter text-white ">Jacob</motion.h1>
-                <motion.h1
-                    initial={{ scale: 0 }} animate={{ scale: 1 }}
-                    className="text-6xl sm:text-8xl md:text-9xl lg:text-9xl font-sans font-medium ml-20 
+            <motion.h1
+                initial={{ scale: 0 }} animate={{ scale: 1 }}
+                className="text-6xl sm:text-8xl md:text-9xl lg:text-9xl font-sans font-medium ml-20 
                     tracking-tighter text-white ">Dietz</motion.h1>
 
-                <ul className="flex">
-                    <MainNavLink text={"Projects"}></MainNavLink>
-                    <MainNavLink text={"GitHub"} to="profile"></MainNavLink>
-                    <MainNavLink text={"Resume"}></MainNavLink>
-                    <MainNavLink text={"Contact"} to="contact"></MainNavLink>
-                </ul>
-            </nav>
+            <ul className="flex">
+                <MainNavLink text={"Projects"} url="/projects"></MainNavLink>
+                <MainNavLink text={"GitHub"} url={"https://github.com/JacobDietz"}></MainNavLink>
+                <MainNavLink text={"Resume"} url="resume.pdf"></MainNavLink>
+                <MainNavLink text={"Contact"} url="/contact"></MainNavLink>
+            </ul>
+        </nav>
     )
 }
 
-export function MainNavLink({ text, to}){
+export function MainNavLink({ text, url }) {
+
+    let externalLink = url?.endsWith(".pdf") || url.startsWith("https")
+
+
     return (
         <motion.li
             whileHover={{ scale: 1.1 }}
             initial={{ scale: 0 }} animate={{ scale: 1 }}
             whileInView={{ opacity: 1 }}
-        
 
-            className=" text-white font-sans text-base sm:text-xl md:text-2xl lg:text-4xl font-bold pl-4 pr-4 
+
+            className=" text-white font-roboto text-base sm:text-xl md:text-2xl lg:text-4xl font-bold pl-4 pr-4 
             underline sm:decoration-1 md:decoration-3 lg:decoration-5 rounded-xl underline-offset-8">
-             <Link to={`/${to}`}>{text}</Link> 
+
+            {externalLink ? (
+                <a href={`${url}`} target="_blank">{text}</a>
+            ) :
+                <Link to={`${url}`}>{text}</Link>
+            }
         </motion.li>
     )
 }
--- frontend/src/background.jsx
+++ frontend/src/background.jsx
@@ -2,22 +2,22 @@
 import './index.css'
 export default function DynamicBckg() {
     return (
-        <div className='fixed inset-0 -z-10 flex items-center justify-center min-h-screen min-w-screen bg-gradient-to-r 
+        <div className='fixed inset-0 -z-10 flex items-center justify-center min-h-screen min-w-screen bg-linear-to-r 
         from-indigo-200 via-zinc-100 to-slate-300' >
             <section className="absolute fifth-layer blur-3xl opacity-90 bg-sky-300 rounded-4xl border-3 border-dotted
-  h-[30dvh] aspect-[1/1]    -translate-x-20 translate-y-20"></section>
+  h-[30dvh] aspect-square    -translate-x-20 translate-y-20"></section>
 
             <section className="fourth-layer absolute blur-3xl opacity-90 bg-amber-300 rounded-4xl  
-  h-[30dvh] aspect-[1/1] -translate-x-20 -translate-y-20"></section>
+  h-[30dvh] aspect-square -translate-x-20 -translate-y-20"></section>
 
             <section className="third-layer absolute blur-3xl opacity-90 bg-lime-600 rounded-4xl 
-   h-[30dvh] aspect-[1/1] translate-x-20 translate-y-20"></section>
+   h-[30dvh] aspect-square translate-x-20 translate-y-20"></section>
 
             <section className="second-layer absolute blur-3xl opacity-90 bg-red-300 rounded-4xl 
-   h-[30dvh] aspect-[1/1] translate-x-20 -translate-y-20"></section>
+   h-[30dvh] aspect-square translate-x-20 -translate-y-20"></section>
 
             <section className="first-layer relative bg-indigo-400  rounded-4xl blur-3xl opacity-90  
-   h-[30dvh] aspect-[1/1] flex items-center justify-center">
+   h-[30dvh] aspect-square flex items-center justify-center">
             </section>
 
         </div>
