6b5930c3339e3658734a9b830bc5a22ad54a6879
Jacob Dietz <jacobdietz2383@gmail.com>
2025-11-14 18:44:35 -0700

    implemented mobile for projects page

--- frontend/src/pages/projectsPage.jsx
+++ frontend/src/pages/projectsPage.jsx
@@ -27,7 +27,7 @@ export default function Projects() {
             </ProjectsTunnel.In>
       <div className="relative w-screen h-screen overflow-hidden">
         <div className="absolute inset-0 z-10 w-full h-full bg-customWhite overflow-hidden">
-          <div className="absolute inset-4 border-2 border-customBlue bg-transparent box-border p-4 flex flex-col">
+          <div className="absolute inset-4 border-2 border-customBlue bg-transparent p-4 flex flex-col">
             <Link
               to="/"
               className="absolute self-end text-customGreen text-lg md:text-4xl hover:text-green-800"
@@ -35,7 +35,7 @@ export default function Projects() {
               Home
             </Link>
 
-            <section className="h-[35%]">
+            <section className="h-[15%] md:h-[35%]">
               <motion.h1
                 initial={{ x: -300, opacity: 1 }}
                 animate={{ x: 0, opacity: 1 }}
@@ -54,8 +54,8 @@ export default function Projects() {
               </motion.h1>
             </section>
 
-            <section className="flex flex-col md:flex-row h-[90%] relative">
-              <div className="flex flex-col justify-center items-center w-1/2 h-full gap-10">
+            <section className="flex flex-col md:flex-row h-[90%] relative items-center">
+              <div className="flex mb-none flex-col justify-center md:items-center w-1/2 h-2/5 w-full items-center">
                 <motion.img
                   whileHover={{ scale: 1.02 }}
                   initial={{ x: -300, opacity: 1 }}
@@ -63,7 +63,7 @@ export default function Projects() {
                   transition={{ type: "spring", stiffness: 50, damping: 20 }}
                   whileInView={{ opacity: 1 }}
                   alt="Pony Express"
-                  className="h-3/5 shadow-[0px_0px_42px_8px_#d5e7f2]  rounded-xl absolute"
+                  className="h-1/4 md:h-3/5 shadow-[0px_0px_42px_8px_#d5e7f2] rounded-xl absolute"
                   src="/ponyExpress.png"
                   onClick={() => {
                     console.log("express clicked");
@@ -73,16 +73,15 @@ export default function Projects() {
               </div>
 
               {/* Tech Card */}
-
               <motion.div
                 initial={{ x: 300, opacity: 1 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{ type: "spring", stiffness: 50, damping: 20 }}
-                className="w-4/5 flex h-full justify-center items-center "
+                className="w-4/5 flex h-1/2 justify-center items-center"
               >
-                <div className="w-3/4 h-3/4 border-2 flex flex-col border-customRed bg-noteCardColor rounded-xl shadow-[0px_0px_10px_0.1px_#154731]">
-                  <div className="w-full h-1/8 border-2 border-t-0 border-customRed  border-x-0 flex justify-start items-center">
-                    <p className="text-2xl text-projectsBG ml-2 text-red-900">
+                <div className="w-full h-9/10 md:w-4/5 md:w-9/10 md:h-full border-2 flex flex-col border-customRed bg-noteCardColor rounded-xl shadow-[0px_0px_10px_0.1px_#154731] ">
+                  <div className="w-full h-1/6 border-2 border-t-0 border-customRed  border-x-0 flex justify-start items-center">
+                    <p className="text-2xl text-projectsBG ml-2 text-red-900 font-bold">
                       Pony Express
                     </p>
                   </div>
@@ -95,14 +94,14 @@ export default function Projects() {
                     <ul className="flex items-center">
                       <a
                         href="/ponyExpress"
-                        className="text-gray-300 text-2xl mx-5 underline hover:text-green-800"
+                        className="text-gray-300 text:md md:text-2xl mx-5 underline hover:text-green-800"
                       >
                         View More
                       </a>
                       <a
                         href="https://github.com/JacobDietz/pony-express"
                         target="blank"
-                        className="text-gray-300 text-2xl mx-5 underline hover:text-green-800"
+                        className="text-gray-300 text-md md:text-2xl mx-5 underline hover:text-green-800"
                       >
                         Github
                       </a>
